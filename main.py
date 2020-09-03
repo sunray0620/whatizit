62f5ddcc6836e1717d03e70a086f2516c5ed8932
Lei Sun <lesun@google.com>
2020-09-03 13:40:57 -0700

    lint and vision-API

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""Main entranace of the app.
+"""
 # Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,9 +15,10 @@
 # limitations under the License.
 
 # [START gae_python38_app]
-from flask import Flask, render_template, request
 from app.my_images import my_images
-
+from flask import Flask
+from flask import render_template
+from flask import request
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -25,25 +28,25 @@ app.register_blueprint(my_images)
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return render_template("home.html")
+  """Return a friendly HTTP greeting."""
+  return render_template('home.html')
 
 
 @app.route('/me')
 def me():
-    """Return a friendly HTTP greeting."""
-    data = dict(request.headers)
-    return data
+  """Return a friendly HTTP greeting."""
+  data = dict(request.headers)
+  return data
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    return 'Sorry, did not find the page. <br/> - GTI Project by GCP-10'
+def page_not_found():
+  return 'Sorry, did not find the page. <br/> - GTI Project by GCP-10'
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+  # This is used when running locally only. When deploying to Google App
+  # Engine, a webserver process such as Gunicorn will serve the app. This
+  # can be configured by adding an `entrypoint` to app.yaml.
+  app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python38_app]
